43b53766d7a34fdce2ee137cfe320c70739b717b
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 19:14:15 -0600

    first commit

---
+++ MartinezP5.py
@@ -0,0 +1,21 @@
+# MartinezP5
+# Programmer: Adelita Martinez
+# Email: amartinez1013@cnm.edu
+# Purpose: provides user capability to play rock scissor paper
+# Python version: 3.12.3
+
+import random 
+
+# Welcome message
+
+# Initialize empty list for storing results
+
+# Define choices and victors
+
+# Determine the winner 
+
+# Loop
+
+# Display results
+
+# Goodbye and thank you
